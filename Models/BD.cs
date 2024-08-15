8f5fc96149a440b5c7aace733707400f32ec646f
URIEL MATIAS SMUCLER <48240113@ort>
2024-08-15 14:32:35 -0300

    Instalacion ok

--- Models/BD.cs
+++ Models/BD.cs
@@ -6,5 +6,5 @@ namespace TP6__JJOO__Blaser_Smucler.Models;
 static public class BD
 {
     private static string _connectionString = @"Server=localhost; DataBase=JJOO; Trusted_Connection=True;";
-    
+     //HACER CONSIGNA
 }
